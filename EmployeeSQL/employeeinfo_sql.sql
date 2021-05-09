43cba69cdb8f9f351b40932490ba45e8ec2d01b0
samantha8990 <79168294+samantha8990@users.noreply.github.com>
2021-05-08 17:22:33 -0700

    Delete employeeinfo_sql.sql

--- EmployeeSQL/employeeinfo_sql.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT "Employees".emp_no,
-	"Employees".last_name,
-	"Employees".first_name,
-	"Employees".sex,
-	"Salaries".salary
-FROM "Employees"
-INNER JOIN "Salaries" on
-"Employees".emp_no="Salaries".emp_no
